1f40ad46b20c29f64ce7e3ffbd062f049c2dc639
Lynn Bradshaw <readyready15728@gmail.com>
2019-04-24 23:21:28 -0400

    Fixing solution for "Are they the same?"

--- 6-kyu/are-they-the-"same"?.py
+++ 6-kyu/are-they-the-"same"?.py
@@ -1,4 +1,7 @@
 def comp(array1, array2):
+    if array1 is None or array2 is None:
+        return False
+
     for x, y in zip(sorted(array1), sorted(array2)):
         if x**2 != y:
             return False
